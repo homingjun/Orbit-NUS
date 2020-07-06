a56cc215199ca3caa2a689acb0d875baff75b626
Reinbowl <marcusngyong@gmail.com>
2020-07-06 17:14:41 +0800

    Added Ventus and DeadEnds

---
+++ Orbit NUS/Assets/Extras/DeadEnd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadEnd : MonoBehaviour
+{
+    public GameObject DeadEndUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DeadEndUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void CloseDeadEndUI()
+    {
+        DeadEndUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
